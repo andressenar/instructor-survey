6a1e378dea502c72ab15cc19f4062219198f22d7
camilo5020 <camilopiamba5020@gmai.com>
2024-11-27 17:45:43 -0500

    actualizar

--- resources/views/survey/form.blade.php
+++ resources/views/survey/form.blade.php
@@ -1,3 +1,5 @@
+
+<script src="https://cdn.tailwindcss.com"></script>
 <form action="{{ route('survey.submit', $survey->id) }}" method="POST">
     @csrf
     <h3>{{ $survey->name }}</h3>
@@ -9,26 +11,27 @@
         @foreach ($survey->questions as $question)
             <div>
                 <h4>{{ $question->question }}</h4>
-
                 @if ($instructors->isEmpty())
                     <p>No hay instructores disponibles para esta encuesta.</p>
                 @else
                     @foreach($instructors as $instructor)
-                        <p><strong>Instructor: {{ $instructor->first_name }} {{ $instructor->last_name }}</strong></p>
-
-                        @if($question->type == 'radio')
-                            @foreach ($question->options as $option)
-                                <input type="radio" name="answers[{{ $instructor->id }}][{{ $question->id }}]" value="{{ $option }}" id="question-{{ $question->id }}-instructor-{{ $instructor->id }}-{{ $option }}">
-                                <label for="question-{{ $question->id }}-instructor-{{ $instructor->id }}-{{ $option }}">{{ $option }}</label>
-                            @endforeach
-                        @elseif($question->type == 'text')
-                            <textarea name="answers[{{ $instructor->id }}][{{ $question->id }}]" id="question-{{ $question->id }}-instructor-{{ $instructor->id }}"></textarea>
-                        @endif
+                        <div class="flex w-full">
+                            <p><strong>Instructor: {{ $instructor->first_name }} {{ $instructor->last_name }}</strong></p>
+                            <div>
+                                @if($question->type == 'radio')
+                                @foreach ($question->options as $option)
+                                        <input type="radio" name="answers[{{ $instructor->id }}][{{ $question->id }}]" value="{{ $option }}" id="question-{{ $question->id }}-instructor-{{ $instructor->id }}-{{ $option }}">
+                                        <label for="question-{{ $question->id }}-instructor-{{ $instructor->id }}-{{ $option }}">{{ $option }}</label>
+                                @endforeach
+                                @elseif($question->type == 'text')
+                                    <textarea name="answers[{{ $instructor->id }}][{{ $question->id }}]" id="question-{{ $question->id }}-instructor-{{ $instructor->id }}"></textarea>
+                                @endif
+                            </div>
+                        </div>
                     @endforeach
                 @endif
             </div>
         @endforeach
     @endif
-
     <button type="submit">Enviar Encuesta</button>
 </form>
