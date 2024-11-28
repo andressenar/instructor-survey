e8bbbad4164d59becd4bc1bed0e1fae792cb7467
Sebastian-balvin-2774315 <sb1181358@gmail.com>
2024-11-28 11:12:11 -0500

    cambios

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -6,18 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Program extends Model
 {
-<<<<<<< HEAD
-    protected $fillable=[
-        'name'
-    ];
-=======
 
     protected $fillable = [
         'code',
         'name',
     ];
 
->>>>>>> 823d9be2625c3e6cc038acca0aa506a82cf72489
     public function courses ()
     {
         return $this->hasMany(Course::class, 'id');
