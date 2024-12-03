1a5694704662fd6c646b7e6fca9b080a4159e560
Andres <restodev@proton.me>
2024-12-02 22:01:41 -0500

    cam2

--- routes/web.php
+++ routes/web.php
@@ -9,22 +9,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('import');
-});
-
-Route::post('import', [ImportController::class, 'import'])->name('import');
-
-// Rutas de autenticación
-
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
-Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
 // Rutas protegidas
 Route::middleware('auth')->group(function () {
-<<<<<<< HEAD
-=======
     Route::get('/admin', function () {
         if (Auth::user()->role !== 'admin') {
             return redirect()->route('survey.show', ['apprenticeId' => Auth::user()->id, 'surveyId' => 1]);
@@ -37,7 +23,6 @@ Route::middleware('auth')->group(function () {
     Route::post('/import', [ImportController::class, 'import'])->name('import');
     Route::get('reports/general/{instructorId}', [ReportController::class, 'showGeneral'])->name('reports.general');
 
->>>>>>> 9598d53 (new2)
     Route::get('/survey/{apprenticeId}/{surveyId}', [SurveyController::class, 'showSurvey'])->name('survey.show');
     Route::post('/survey/{surveyId}/submit', [SurveyController::class, 'storeAnswers'])->name('survey.submit');
     Route::get('/survey/complete', [SurveyController::class, 'complete'])->name('survey.complete');
